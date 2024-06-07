51fee66eeb146845914efcd674428e8f9f9c8dda
bayram98 <bayramguwanjow@gmail.com>
2024-06-07 15:18:45 +0900

    fix: use call method, and reentrancy check

--- contracts/MultiSender.sol
+++ contracts/MultiSender.sol
@@ -6,36 +6,43 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
 // Use SafeERC20 library for IERC20Upgradeable
 using SafeERC20 for IERC20Upgradeable;
 
 // Define the contract
-contract MultiSender is Initializable {
+contract MultiSender is Initializable, ReentrancyGuardUpgradeable {
     // Initialize the contract
-    function initialize() public initializer {}
+    function initialize() public initializer {
+        __ReentrancyGuard_init();
+    }
 
     // Event definitions
     event SendETH(address[] recipients, uint256[] amounts);
     event SendERC20(address token, address[] recipients, uint256[] amounts);
 
     // Function to send ETH to multiple recipients
-    function sendETH(address[] calldata _recipients, uint256[] calldata _amounts) external payable {
+    function sendETH(
+        address[] calldata _recipients,
+        uint256[] calldata _amounts
+    ) external payable nonReentrant {
         // Check if the lengths of recipients and amounts arrays are equal
         require(_recipients.length == _amounts.length, "Must have the same length");
 
         // Calculate the total amount to be sent
-        uint256 totalAmount = 0;
+        uint256 _totalAmount = 0;
         for (uint256 i = 0; i < _amounts.length; i++) {
-            totalAmount += _amounts[i];
+            _totalAmount += _amounts[i];
         }
 
         // Check if the sender has enough ETH
-        require(msg.value == totalAmount, "Unequal transfer amount");
+        require(msg.value == _totalAmount, "Unequal transfer amount");
 
         // Send ETH to recipients
         for (uint256 i = 0; i < _recipients.length; i++) {
-            payable(_recipients[i]).transfer(_amounts[i]);
+            (bool success, ) = payable(_recipients[i]).call{value: _amounts[i]}("");
+            require(success, "Transfer failed");
         }
 
         // Emit the SendETH event
@@ -44,31 +51,37 @@ contract MultiSender is Initializable {
 
     // Function to send ERC20 tokens to multiple recipients
     function sendERC20(
-        address _token,
+        address _tokenAddress,
         address[] calldata _recipients,
         uint256[] calldata _amounts
-    ) external {
+    ) external nonReentrant {
         // Check if the lengths of recipients and amounts arrays are equal
         require(_recipients.length == _amounts.length, "Must have the same length");
 
         // Calculate the total amount to be sent
-        uint256 totalAmount = 0;
+        uint256 _totalAmount = 0;
         for (uint256 i = 0; i < _amounts.length; i++) {
-            totalAmount += _amounts[i];
+            _totalAmount += _amounts[i];
         }
 
         // Create an instance of the ERC20 token contract
-        IERC20Upgradeable token = IERC20Upgradeable(_token);
+        IERC20Upgradeable _token = IERC20Upgradeable(_tokenAddress);
+
+        // Check if the sender has approved the contract to spend the tokens
+        require(
+            _token.allowance(msg.sender, address(this)) >= _totalAmount,
+            "Transfer not approved"
+        );
 
         // Check if the sender has enough tokens
-        require(token.balanceOf(msg.sender) >= totalAmount, "Not enough tokens");
+        require(_token.balanceOf(msg.sender) >= _totalAmount, "Not enough tokens");
 
         // Send tokens to each recipient
         for (uint256 i = 0; i < _recipients.length; i++) {
-            token.transferFrom(msg.sender, _recipients[i], _amounts[i]);
+            _token.transferFrom(msg.sender, _recipients[i], _amounts[i]);
         }
 
         // Emit the SendERC20 event
-        emit SendERC20(_token, _recipients, _amounts);
+        emit SendERC20(_tokenAddress, _recipients, _amounts);
     }
 }
